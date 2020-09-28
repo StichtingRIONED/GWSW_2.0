a8898632b9e9394d3ec9af88a32e5a01da2f7ffc
Stichting RIONED <43248274+StichtingRIONED@users.noreply.github.com>
2020-09-28 18:20:42 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,40 @@
+var respecConfig =
+{
+  //specStatus: "unofficial",
+  specStatus: "unofficial",
+  //specType: "HR",                     // Geonovum: Handreiking
+  //format: "markdown",                 // Geonovum: altijd "markdown"     
+  shortName: "GWSW-2.0", 	            // Wordt gebruikt in de document URL
+  license: "cc0",
+   
+  editors: [{
+    name: "Michel Böhms",
+    url: "mailto:michel.bohms@tno.nl",
+    company: "TNO",
+    companyURL: "https://www.tno.nl"
+  },
+  {
+    name: "Eric Oosterom",
+    url: "mailto:eric.oosterom@rioned.org",
+    company: "Stichting RIONED",
+    companyURL: "https://www.riool.net"
+  }],
+  authors: [{
+    name: "Marinus Vonhof",
+    url: "mailto:marinus.vonhof@rioned.org",
+    company: "Stichting RIONED",
+    companyURL: "https://www.riool.net"
+  }],
+  otherLinks: [{
+    key: "GitHub", href: "https://github.com/StichtingRIONED/GWSW"
+  }],
+  //github: "https://github.com/StichtingRIONED/GWSW"
+  //-- Repositorynaam/issues op GitHub ------------------------------------------------
+  //issueBase: "https://github.com/StichtingRIONED/GWSW/issues/",
+
+  thisVersion: "https://rioned.github.io/gwsw",
+  latestVersion: "", // gepubliceerde versie
+  edDraftURI: "https://rioned.github.io/gwsw", // werkversie
+
+  maxTocLevel: 3, // Aantal niveau's ToC, default is 0
+};
